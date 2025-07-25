8986bc8dad5eb50e720235026ae8d82c18bb6420
amir ahrar <amirahrar.dev@gmail.com>
2025-07-25 18:51:01 +0330

    Update ماشین حساب.py

--- ماشین حساب.py
+++ ماشین حساب.py
@@ -1,1 +1,16 @@
-
+while True:
+  a=int(input("عدد اول را وارد کنید :"))
+  print("عملیات های قابل قبول : + , - , * , / , exit")
+  op=input("عملیات خودرا وارد کنید :")
+  if op=="exit":break
+  b=int(input("عدد دوم را وارد کنید :"))
+  if op=="+":
+    print(a+b)
+  elif op=="-":
+    print(a-b)
+  elif op=="*":
+    print(a*b)
+  elif op=="/":
+    print(a/b)
+  else:
+    print("عملیات مورد نظر یافت نشد لطفا دوباره وارد کنید")
